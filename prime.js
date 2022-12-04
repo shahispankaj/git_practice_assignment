fc82144e9b10232f184d07c18061d600e9b23529
unknown <richakashyap225@gmail.com>
2022-12-05 00:46:07 +0530

    pankaj completed

--- prime.js
+++ prime.js
@@ -1,14 +1,14 @@
 let num=13;
-let count=1;
+let count=0;
 
-for(i=1; i<=13; i++){
+for(i=1; i<=num; i++){
   if(num%i==0){
     count++;
   }
 
 }
 
-if(count==1){
+if(count==2){
   console.log(num, "is prime")
 }else{
   console.log(num,"is not prime")
